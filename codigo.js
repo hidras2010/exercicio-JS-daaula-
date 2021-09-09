97cfea3a0abb91785cc9cef12038fe432f3e1fb6
hidras2010 <hidras2010ht@gmail.com>
2021-09-09 17:58:12 -0300

    Alteração

--- codigo.js
+++ codigo.js
@@ -7,12 +7,38 @@ function add(){
     var listaS = document.createElement("li")
 
     listaU.textContent ="Nome do usuario: "+ usuario
-    listaS.textContent ="Senha do usuario:"+ senha
+    listaS.textContent ="E-mail do usuario: "+ senha
 
-    lista.appendChild(listaU)
-    lista.appendChild(listaS)
+    
+    if(verifica()===true){
+        lista.appendChild(listaU)
+        lista.appendChild(listaS)
+    }
 
 
 
 
+}
+function verifica(){
+    let usuario = document.getElementById("usuario").value
+    let senha = document.getElementById("senha").value
+
+
+    //fazendo condicionais
+    if(usuario===""){
+        alert("Por favor inserir o seu nome")
+        return false
+    }else if(usuario.length < 3){
+        alert("insera um nome valido")
+        return false
+    }else if(senha===""){
+        alert("Por favor inserir o seu E-mail")
+        return false
+    }else if(senha.length < 3){
+        alert("Introduza um E-mail valido")
+        
+
+    }
+    
+        return true
 }
